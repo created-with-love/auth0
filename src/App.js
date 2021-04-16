e3acc4e14b1fef573eb2375821efb968ad03d685
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2021-04-16 10:38:57 +0300

    add ready project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+import { css } from '@emotion/react';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+import ClipLoader from 'react-spinners/ClipLoader';
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 function App() {
-  return <div className="App"></div>;
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return (
+      <ClipLoader
+        color={`#ffffff`}
+        loading={isLoading}
+        css={override}
+        size={150}
+      />
+    );
+  }
+
+  return (
+    <>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </>
+  );
 }
 
 export default App;
